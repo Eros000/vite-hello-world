891f47dc89a8294f77a7be295b1cb110e14d0cfb
Eros000 <e.ghilardi@hotmail.it>
2023-07-12 17:37:45 +0200

    AppTitle Created

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+  data() {
+    return { 
+
+    }
+  },
+}
+</script>
+
+<template>
+
+    <h1>
+        Hello world!
+    </h1>
+  
+</template>
+
+<style scoped>
+
+h1{
+    text-align: center;
+    font-size: 3em;
+    color: brown;
+}
+
+</style>
